d2cf88b6e0dad4ee01c562ba2f4691048e24f3e2
cjmanetta <cjmanetta@gmail.com>
2015-12-09 15:17:38 -0800

    add a lot

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,3 +2,11 @@
 //= require jquery
 //= require javascripts/semantic
 
+$('.ui.sticky')
+  .sticky({
+    context: '.context',
+    pushing: true,
+    offset       : 50,
+    bottomOffset : 50,
+  })
+;
