c3cf41478a907cf7924027c01f0dc0eae4ed6633
cjmanetta <cjmanetta@gmail.com>
2016-01-08 14:35:49 -0800

    overhaul responsive styling

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,12 +1,3 @@
 //= require_tree .
 //= require jquery
 //= require javascripts/semantic
-
-$('.ui.sticky')
-  .sticky({
-    context: '.context',
-    pushing: true,
-    offset       : 50,
-    bottomOffset : 50,
-  })
-;
